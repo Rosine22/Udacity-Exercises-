92ef34c54bc54605f63b15db72f661e7801a101b
Rosine22 <116634586+Rosine22@users.noreply.github.com>
2024-11-14 15:14:40 +0000

    midRange

---
+++ midRange.js
@@ -0,0 +1,8 @@
+//Write a function midrange, that calculates the midrange of 3 numbers. The midrange is the mean of the smallest and largest number.
+
+function midrange(a,b,c) {
+    let min = Math.min(a,b,c) ;
+    let max = Math.max(a,b,c) ;
+    return (min+max)/2 ;
+    }
+    console.log(midrange(3,10,9)); // midRange=6.5
